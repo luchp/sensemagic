93d3a9b1fa5d9b3eb7d234be403d4f3cc1e26c72
luc <luc@sensemagic.nl>
2026-08-11 14:50:08 +0200

    run

--- app/run.sh
+++ app/run.sh
@@ -4,5 +4,5 @@
 cd /home/projects/sensemagic/app
 
 # Start FastAPI with uvicorn
-uv run python -m uvicorn main:app --host 0.0.0.0 --port 9000
+/home/projects/.local/bin/uv run python -m uvicorn main:app --host 0.0.0.0 --port 9000
 
